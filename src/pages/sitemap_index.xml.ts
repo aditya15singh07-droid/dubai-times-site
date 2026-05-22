0487cc2387c72ef73fcc8e1804dcf4419bc072f9
Aditya Singh <aditya15singh07@gmail.com>
2026-05-22 10:33:24 +0530

    Add sitemap index for search console

---
+++ src/pages/sitemap_index.xml.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+
+const siteUrl = "https://dubai-time.com";
+
+export const GET: APIRoute = () => {
+  const lastmod = new Date().toISOString();
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${siteUrl}/sitemap.xml</loc>
+    <lastmod>${lastmod}</lastmod>
+  </sitemap>
+</sitemapindex>
+`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+};
